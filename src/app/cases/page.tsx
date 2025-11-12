a7800883c982c266c387eb32f344479f560d4e22
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-11-12 12:26:27 -0300

    fix: add width and height attributes to WixMediaImage for better image rendering

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -23,7 +23,7 @@ export default async function Cases() {
          <div className="grid grid-cols-1 md:grid-cols-2 gap-8" data-lag="0.1">
             {cases.map((item) => (
                <Link key={item.id} className="flex flex-col items-center w-full h-[50vh] md:h-[70vh] shadow-lg rounded-3xl" href={`/cases/${item.slug}`}>
-                  <WixMediaImage media={item.brandLogo} alt={item.projectName} objectFit="cover" imageTitle projectName={item.projectName} companyName={item.companyName} />
+                  <WixMediaImage media={item.brandLogo} alt={item.projectName} objectFit="cover" imageTitle projectName={item.projectName} companyName={item.companyName} width={1725} height={900} />
                </Link>
             ))}
          </div>
