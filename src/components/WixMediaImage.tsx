c2e2ab1f37ebc1e754bdcf006911d490031f4928
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-11-12 12:25:31 -0300

    refactor: simplify image URL retrieval in WixMediaImage component

--- src/components/WixMediaImage.tsx
+++ src/components/WixMediaImage.tsx
@@ -1,63 +1,63 @@
 import { media as wixMedia } from '@wix/sdk';
 import Image, { ImageProps } from 'next/image';
 
-function getImageUrlForMedia(media: string, width: number, height: number) {
-  if (media.startsWith('wix:image')) {
-    return wixMedia.getScaledToFillImageUrl(media, width, height, {});
-  } else {
-    return media;
-  }
+function getImageUrlForMedia(media: string) {
+   if (media.startsWith('wix:image')) {
+      console.log(wixMedia.getImageUrl(media));
+      return wixMedia.getImageUrl(media).url;
+   } else {
+      return media;
+   }
 }
 
 export function WixMediaImage({
-  media,
-  height = 900,
-  width = 1725,
-  alt = 'no info available for image',
-  className,
-  objectFit,
-  disableZoom = false,
-  imageTitle = false,
-  projectName,
-  companyName,
+   media,
+   height = 900,
+   width = 1725,
+   alt = 'no info available for image',
+   className,
+   objectFit,
+   disableZoom = false,
+   imageTitle = false,
+   projectName,
+   companyName,
 }: {
-  media?: string;
-  alt?: string;
-  width?: number;
-  height?: number;
-  className?: string;
-  projectName?: string;
-  companyName?: string;
-  disableZoom?: boolean;
-  imageTitle?: boolean;
-  objectFit?: 'cover' | 'contain';
+   media?: string;
+   alt?: string;
+   width?: number;
+   height?: number;
+   className?: string;
+   projectName?: string;
+   companyName?: string;
+   disableZoom?: boolean;
+   imageTitle?: boolean;
+   objectFit?: 'cover' | 'contain';
 }) {
-  const imageUrl = media
-    ? getImageUrlForMedia(media || '', width, height)
-    : '';
+   const imageUrl = media
+      ? getImageUrlForMedia(media)
+      : '';
 
-  const styleProps: Partial<ImageProps> = {
-    ...(objectFit
-      ? { style: { objectFit }, width, height}
-      : { width, height }),
-  };
+   const styleProps: Partial<ImageProps> = {
+      ...(objectFit
+         ? { style: { objectFit }, width, height }
+         : { width, height }),
+   };
 
-  return (
-    <div className={`relative flex flex-col items-center justify-center h-full w-full shrink-0 group overflow-hidden rounded-3xl`}>
-      <Image
-        {...styleProps}
-        src={imageUrl}
-        alt={alt}
-        className={`w-full h-full ${
-          !disableZoom ? 'group-hover:scale-110' : ''
-        } transition duration-600 ease-in-out ${className}`}
-      />
-      {imageTitle && (
-        <div className="flex justify-between rounded-3xl py-2 px-6 absolute bottom-5 md:bottom-0 md:translate-y-full group-hover:-translate-y-full transition-all duration-600 w-[90%] md:overflow-hidden bg-white/70 backdrop-blur-sm inset-ring inset-ring-gray-700/10">
-          <span>{projectName}</span>
-          <span>{companyName}</span>
-        </div>
-      )}
-    </div>
-  );
+   return (
+      <div className={`relative flex flex-col items-center justify-center h-full w-full shrink-0 group overflow-hidden rounded-3xl`}>
+         <Image
+            {...styleProps}
+            src={imageUrl}
+            alt={alt}
+            className={`w-full h-full ${!disableZoom ? 'group-hover:scale-110' : ''
+               } transition duration-600 ease-in-out ${className}`}
+         />
+         {imageTitle && (
+            <div className="flex justify-between rounded-3xl py-2 px-6 absolute bottom-5 md:bottom-0 md:translate-y-full group-hover:-translate-y-full transition-all duration-600 w-[90%] md:overflow-hidden bg-white/70 backdrop-blur-sm inset-ring inset-ring-gray-700/10">
+               <span>{projectName}</span>
+               <span>{companyName}</span>
+            </div>
+         )}
+      </div>
+   );
 }
