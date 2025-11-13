653bedcaac8647c0787ffc4e551d449d0482d173
Leonardo Brum <leonardobrum.dev@gmail.com>
2025-11-13 15:08:12 -0300

    fix: move accept-language header to locale service

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,8 @@
 import { getRequestConfig } from "next-intl/server";
 import { getUserLocale } from "@/services/locale";
-import { headers } from "next/headers";
-import { defaultLocale } from "./config";
 
 export default getRequestConfig(async () => {
-   const headersList = await headers();
-   const acceptLanguage = headersList.get('accept-language');
-   let locale = defaultLocale;
-   
-   if(acceptLanguage?.toLowerCase().includes('pt')) {
-      locale = 'pt-BR';
-   }
+   const locale = await getUserLocale();
 
    return {
       locale,
--- src/services/locale.ts
+++ src/services/locale.ts
@@ -1,12 +1,20 @@
 'use server'
 
 import { defaultLocale, Locale } from "@/i18n/config";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 const COOKIE_NAME = 'USER_LOCALE';
 
 export async function getUserLocale() {
-   return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
+   const headersList = await headers();
+   const acceptLanguage = headersList.get('accept-language');
+   let locale = defaultLocale;
+   
+   if(acceptLanguage?.toLowerCase().includes('pt')) {
+      locale = 'pt-BR';
+   }
+
+   return (await cookies()).get(COOKIE_NAME)?.value || locale;
 }
 
 export async function setUserLocale(locale: Locale) {
